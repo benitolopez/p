064e5b385413e2680bbbea07c4c3b0aa8b2275ab
Benito Lopez <hello@lopezb.com>
2016-11-12 03:25:35 +0100

    fix indentation

--- p.sh
+++ p.sh
@@ -34,13 +34,13 @@ function print_help() {
     echo "Usage: p <option>"
     echo ""
     echo "Options:"
-	echo "  <project name>           Run project(s) configuration script."
+    echo "  <project name>           Run project(s) configuration script."
     echo "  add <project name>       Add project."
     echo "  list                     List all projects."
     echo "  delete <project name>    Delete project(s)."
     echo "  o <project name>         Open project(s) with the default editor."
     echo "  edit <project name>      Edit project(s)."
-	echo "  rename <old> <new>       Rename project."
+    echo "  rename <old> <new>       Rename project."
     echo "  -h --help                Display this information."
     echo "  -v --version             Display version info."
     echo ""
@@ -48,7 +48,7 @@ function print_help() {
 
 # Check if project has a valid name (not reserved)
 function check_project_name() {
-	case "$1" in
+    case "$1" in
         add|list|delete|edit|rename|o|-h|--help|-v|--version)
             echo "$1: Illegal name"
             return 1
@@ -66,7 +66,7 @@ function check_editor() {
 
 # Open the configuration file with the default editor
 function edit_config_file() {
-	$EDITOR "$p_dir/$1.sh"
+    $EDITOR "$p_dir/$1.sh"
 }
 
 # Open the project with the default editor
@@ -107,17 +107,17 @@ function _p() {
     done
 
     case "$1" in
-    	add)
-    		# Add project
+        add)
+            # Add project
             if [ "$2" ]; then
                 for argument in "${@:2}"; do
                     if [ -f "$p_dir/$argument.sh" ]; then
                         print_error 1 "Project $argument already exists"
                         return 1
-    				else
-    					check_project_name "$argument" || return
-    					printf "#!/usr/bin/env bash\n\n# This script runs when you call the project with \"p %s\"\n\n# DO NOT DELETE THIS LINE\nPROJECT_PATH=$PWD\n\n" "$argument" > "$p_dir/$argument.sh"
-    					echo "Added project $argument"
+                    else
+                        check_project_name "$argument" || return
+                        printf "#!/usr/bin/env bash\n\n# This script runs when you call the project with \"p %s\"\n\n# DO NOT DELETE THIS LINE\nPROJECT_PATH=$PWD\n\n" "$argument" > "$p_dir/$argument.sh"
+                        echo "Added project $argument"
                     fi
                 done
             else
@@ -125,35 +125,35 @@ function _p() {
                 return 1
             fi
             ;;
-    	list)
-    		# List projects
-    		if [ ${#PROJECTS[@]} -eq 0 ]; then
-    			print_error 1 "There are no projects"
+        list)
+            # List projects
+            if [ ${#PROJECTS[@]} -eq 0 ]; then
+                print_error 1 "There are no projects"
                 return 1
-    		else
-    			for project in "${PROJECTS[@]}"; do
-    				echo $project
-    			done
-    		fi
-    		;;	
-    	delete)
+            else
+                for project in "${PROJECTS[@]}"; do
+                    echo $project
+                done
+            fi
+            ;;  
+        delete)
             # Remove project
             if [ "$2" ]; then
                 for argument in "${@:2}"; do
-    				if [ -f "$p_dir/$argument.sh" ]; then
-    					rm "$p_dir/$argument.sh"
-    					echo "Project $argument deleted"
-    				else
-    					print_error 1 "Project $argument does not exist"
-    					return 1
-    				fi
+                    if [ -f "$p_dir/$argument.sh" ]; then
+                        rm "$p_dir/$argument.sh"
+                        echo "Project $argument deleted"
+                    else
+                        print_error 1 "Project $argument does not exist"
+                        return 1
+                    fi
                 done
             else
                 print_error 1 "No project name given"
                 return 1
             fi
             ;;
-    	edit)
+        edit)
             # Edit project
             if [ "$2" ]; then
                 for argument in "${@:2}"; do
@@ -212,7 +212,7 @@ function _p() {
                 return 1
             fi
             ;;
-    	-h|--help)
+        -h|--help)
             # Show help screen
             print_help
             ;;
@@ -220,19 +220,19 @@ function _p() {
             # Show version
             echo "P $VERSION."
             ;;
-    	*)
-    		# Empty command
+        *)
+            # Empty command
             if [ -z "$1" ]; then
                 print_help
-    		# Run the project configuration file
+            # Run the project configuration file
             else
-    			for argument in "${@:1}"; do
-    				if [ -f "$p_dir/$argument.sh" ]; then
-    					sh $p_dir/$argument.sh
-    				else
-    					print_error 1 "Project $argument does not exist"
-    				fi
-    			done
+                for argument in "${@:1}"; do
+                    if [ -f "$p_dir/$argument.sh" ]; then
+                        sh $p_dir/$argument.sh
+                    else
+                        print_error 1 "Project $argument does not exist"
+                    fi
+                done
             fi
             ;;
     esac
