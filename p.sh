dc32d4df8991d41c5ae350fb32ea167a5533081e
Benito Lopez <hello@lopezb.com>
2016-12-16 01:17:44 +0100

    Prefix functions

--- p.sh
+++ p.sh
@@ -18,7 +18,7 @@
 #             - default '$EDITOR .'
 
 # Print error message and return error code
-function print_error() {
+_p_print_error() {
     if [ "$2" ]; then
         echo "$2"
     fi
@@ -30,7 +30,7 @@ function print_error() {
 }
 
  # Print help screen
-function print_help() {
+_p_print_help() {
     echo "Usage: p <option>"
     echo ""
     echo "Options:"
@@ -48,7 +48,7 @@ function print_help() {
 }
 
 # Check if project has a valid name (not reserved)
-function check_project_name() {
+_p_check_project_name() {
     case "$1" in
         add|list|delete|edit|rename|o|-h|--help|-v|--version)
             echo "$1: Illegal name"
@@ -57,7 +57,7 @@ function check_project_name() {
 }
 
 # Check if we have the editor environment variable
-function check_editor() {
+_p_check_editor() {
     if [ -z "$EDITOR" ]; then
         echo "The \$EDITOR environment variable is not set."
         echo "You need to manually edit the configuration file in $p_dir."
@@ -66,39 +66,39 @@ function check_editor() {
 }
 
 # Open the configuration file with the default editor
-function edit_config_file() {
+_p_edit_config_file() {
     $EDITOR "$p_dir/$1.sh"
 }
 
 # Open the project with the default editor
-function open_with_editor() {
-    path=$(sed -n "/^PROJECT_PATH=\(.*\)$/s//\1/p" $p_dir/$1.sh)
+_p_open_with_editor() {
+    p_path=$(sed -n "/^PROJECT_PATH=\(.*\)$/s//\1/p" $p_dir/$1.sh)
 
-    if [ -d "$path" ]; then
-        cd $path
+    if [ -d "$p_path" ]; then
+        cd $p_path
         # Open the folder - You can override this function with $_P_OPEN_FUNC
         ${_P_OPEN_FUNC:-$EDITOR .}
     else
-        print_error 1 "Directory does not exist"
+        _p_print_error 1 "Directory does not exist"
         return 1
     fi
 }
 
 # Go to the project directory
-function go_to_project() {
-    path=$(sed -n "/^PROJECT_PATH=\(.*\)$/s//\1/p" $p_dir/$1.sh)
+_p_go_to_project() {
+    p_path=$(sed -n "/^PROJECT_PATH=\(.*\)$/s//\1/p" $p_dir/$1.sh)
 
-    if [ -d "$path" ]; then
+    if [ -d "$p_path" ]; then
         # Go to the project directory
-        cd $path
+        cd $p_path
     else
-        print_error 1 "Directory does not exist"
+        _p_print_error 1 "Directory does not exist"
         return 1
     fi
 }
 
-function _p() {
-    VERSION=2.0.0
+_p() {
+    VERSION=3.0.0
 
     # Set projects dir (can be changed with the environment variable $_P_DIR)
     p_dir="${_P_DIR:-$HOME/.projects}"
@@ -126,23 +126,23 @@ function _p() {
             if [ "$2" ]; then
                 for argument in "${@:2}"; do
                     if [ -f "$p_dir/$argument.sh" ]; then
-                        print_error 1 "Project $argument already exists"
+                        _p_print_error 1 "Project $argument already exists"
                         return 1
                     else
-                        check_project_name "$argument" || return
+                        _p_check_project_name "$argument" || return
                         printf "#!/usr/bin/env bash\n\n# This script runs when you call the project with \"p %s\"\n\n# DO NOT DELETE THIS LINE\nPROJECT_PATH=$PWD\n\n" "$argument" > "$p_dir/$argument.sh"
                         echo "Added project $argument"
                     fi
                 done
             else
-                print_error 1 "No project name given"
+                _p_print_error 1 "No project name given"
                 return 1
             fi
             ;;
         list)
             # List projects
             if [ ${#PROJECTS[@]} -eq 0 ]; then
-                print_error 1 "There are no projects"
+                _p_print_error 1 "There are no projects"
                 return 1
             else
                 for project in "${PROJECTS[@]}"; do
@@ -158,12 +158,12 @@ function _p() {
                         rm "$p_dir/$argument.sh"
                         echo "Project $argument deleted"
                     else
-                        print_error 1 "Project $argument does not exist"
+                        _p_print_error 1 "Project $argument does not exist"
                         return 1
                     fi
                 done
             else
-                print_error 1 "No project name given"
+                _p_print_error 1 "No project name given"
                 return 1
             fi
             ;;
@@ -172,15 +172,15 @@ function _p() {
             if [ "$2" ]; then
                 for argument in "${@:2}"; do
                     if [ -f "$p_dir/$argument.sh" ]; then
-                        check_editor || return
-                        edit_config_file $argument
+                        _p_check_editor || return
+                        _p_edit_config_file $argument
                     else
-                        print_error 1 "Project $argument does not exist"
+                        _p_print_error 1 "Project $argument does not exist"
                         return 1
                     fi
                 done
             else
-                print_error 1 "No project name given"
+                _p_print_error 1 "No project name given"
                 return 1
             fi
             ;;
@@ -188,19 +188,19 @@ function _p() {
             # Rename project
             if [ "$2" ]; then
                 if [ ! -f "$p_dir/$2.sh" ]; then
-                    print_error 1 "Project $2 does not exist"
+                    _p_print_error 1 "Project $2 does not exist"
                     return 1
                 else
                     if [ "$3" ]; then
                         if [ -f "$p_dir/$3.sh" ]; then
-                            print_error 1 "Project $3 already exists"
+                            _p_print_error 1 "Project $3 already exists"
                             return 1
                         else
                             mv "$p_dir/$2.sh" "$p_dir/$3.sh"
                             echo "Project renamed to $3"
                         fi
                     else
-                        print_error 1 "No new name given"
+                        _p_print_error 1 "No new name given"
                         return 1
                     fi
                 fi
@@ -214,15 +214,15 @@ function _p() {
             if [ "$2" ]; then
                 for argument in "${@:2}"; do
                     if [ -f "$p_dir/$argument.sh" ]; then
-                        check_editor || return
-                        open_with_editor $argument
+                        _p_check_editor || return
+                        _p_open_with_editor $argument
                     else
-                        print_error 1 "Project $argument does not exist"
+                        _p_print_error 1 "Project $argument does not exist"
                         return 1
                     fi
                 done
             else
-                print_error 1 "No project name given"
+                _p_print_error 1 "No project name given"
                 return 1
             fi
             ;;
@@ -230,19 +230,19 @@ function _p() {
             # Go to the project directory
             if [ "$2" ]; then
 				if [ -f "$p_dir/$2.sh" ]; then
-					go_to_project $2
+					_p_go_to_project $2
 				else
-					print_error 1 "Project $2 does not exist"
+					_p_print_error 1 "Project $2 does not exist"
 					return 1
 				fi
             else
-                print_error 1 "No project name given"
+                _p_print_error 1 "No project name given"
                 return 1
             fi
             ;;
         -h|--help)
             # Show help screen
-            print_help
+            _p_print_help
             ;;
         -v|--version)
             # Show version
@@ -251,14 +251,14 @@ function _p() {
         *)
             # Empty command
             if [ -z "$1" ]; then
-                print_help
+                _p_print_help
             # Run the project configuration file
             else
                 for argument in "${@:1}"; do
                     if [ -f "$p_dir/$argument.sh" ]; then
                         . $p_dir/$argument.sh
                     else
-                        print_error 1 "Project $argument does not exist"
+                        _p_print_error 1 "Project $argument does not exist"
                     fi
                 done
             fi
